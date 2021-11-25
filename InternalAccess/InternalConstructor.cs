2b7dce6adff0ffc1db17fff53ea9378b07a38a1a
michael Steele <michael.steele@checkout.com>
2021-11-25 00:00:10 +0000

    First Commit

---
+++ InternalAccess/InternalConstructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace InternalAccess
+{
+    public static class InternalConstructor
+    {
+        private const int ParameterCount = 1;
+        public static object ConstructSingleParameter<T>(object parameter)
+        {
+            var ctor = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)
+                .FirstOrDefault(c => c.GetParameters().Any());
+
+            return (T)ctor?.Invoke(new object[ParameterCount]
+                { parameter });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Internal Constructor
+
+The internal constructor uses reflection to instantiate a class with an internal constructor. The example allows for a single parameter.
